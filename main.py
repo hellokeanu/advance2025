eb53ffb7e72bcad816a1c8813c8eecf5140877df
hellokeanu <hello.keanu@gmail.com>
2025-11-13 16:43:13 +0900

    20251113

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-# 샘플 Python 스크립트입니다.
+import json
+import pandas as pd
+import requests
+import urllib
+from urllib.parse import urlencode, unquote, quote_plus
+from urllib.request import urlopen
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+pd.set_option('display.width', 5000)
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
 
-# hello code bong song song bong
+Key='835f42a79d2e4e18a5d684b7f97aeedc'
+HourlyPresent='/current'
+BasicUrl='https://api.weatherbit.io/v2.0'
+Hours = 48
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+def weatherbittest(LAT,LNG):
+    params = '?' + urlencode({quote_plus("lat"): str(LAT),
+                              quote_plus("lon"): str(LNG),
+                              quote_plus("key"): Key,
+                              quote_plus("include"): 'minutely'})
+    FinalURL = BasicUrl + HourlyPresent + unquote(params)
+    req = urllib.request.Request(FinalURL)
+    response_body = urlopen(req).read()
+    data = json.loads(response_body)
 
+    DF = pd.DataFrame.from_dict(data['data'])
+
+    return DF
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    LAT = 35.002707049416614
+    LNG = 125.73350758529737
+    Output=weatherbittest(LAT,LNG)
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+    print(Output)
+
+
